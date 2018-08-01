2dfea6dc6c10b62ce4a2e5f4e857964697e3f8b1
eduardo <eduardo27_minotauro@hotmail.com>
2018-08-01 14:59:55 -0300

    atualizar

--- src/test/java/br/com/controller/ContatoControllerTest.java
+++ src/test/java/br/com/controller/ContatoControllerTest.java
@@ -2,7 +2,6 @@ package br.com.controller;
 
 import br.com.AbstractTest;
 import br.com.modelo.ContatoEntity;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import net.minidev.json.JSONObject;
 import org.apache.commons.io.IOUtils;
 import org.assertj.core.api.Assertions;
@@ -30,7 +29,7 @@ public class ContatoControllerTest extends AbstractTest{
 
     @Test
     public void findAllContato() throws Exception {
-       saveV2();
+       saveAll();
        this.mockMvc.perform(MockMvcRequestBuilders.get(PATH+"contatos"))
                .andExpect(MockMvcResultMatchers.status().isOk())
                .andDo(MockMvcResultHandlers.print());
@@ -53,7 +52,7 @@ public class ContatoControllerTest extends AbstractTest{
 
     @Test
     public void updateContato() throws Exception {
-        ContatoEntity edit = saveV1();
+        ContatoEntity edit = saveOneContato();
         int id = Integer.parseInt(valueOf(edit.getId()));
 
         Map<String, String> data = new HashMap<>();
@@ -79,7 +78,7 @@ public class ContatoControllerTest extends AbstractTest{
     @Test
     public void deleteContato() throws Exception {
 
-        ContatoEntity created = saveV1();
+        ContatoEntity created = saveOneContato();
         Assertions.assertThat(created.getId()).isNotNull();
 
         String id = valueOf(created.getId());
@@ -100,14 +99,14 @@ public class ContatoControllerTest extends AbstractTest{
         Assertions.assertThat(id).isNotNull();
     }
 
-    private ContatoEntity saveV1(){
+    private ContatoEntity saveOneContato(){
         ContatoEntity contato = new ContatoEntity("Luis", "(34)32345710", "luis@gmail.com");
         return repository.save(contato);
     }
 
-    private void saveV2(){
+    private void saveAll(){
 
-        ContatoEntity contato1 = new ContatoEntity("Lucas", "(34)32225510", "lucas@gmail.com");
+        ContatoEntity contato1 = new ContatoEntity("Lucas", "(34)32225587", "lucas@gmail.com");
         repository.save(contato1);
 
         ContatoEntity contato2 = new ContatoEntity("Mara", "(34)32223412", "mara@gmail.com");
@@ -119,7 +118,7 @@ public class ContatoControllerTest extends AbstractTest{
         ContatoEntity contato4 = new ContatoEntity("Matheus", "(34)32244015", "matheus@gmail.com");
         repository.save(contato4);
 
-        ContatoEntity contato5 = new ContatoEntity("Marcos", "(34)322785017", "carcos@gmail.com");
+        ContatoEntity contato5 = new ContatoEntity("Marcos", "(34)322785017", "marcos@gmail.com");
         repository.save(contato5);
     }
 
